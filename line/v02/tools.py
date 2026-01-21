d9989cfa2e9da21b0b813b8e66921fb0d37b036d
Lucy Liu <masssb1@gmail.com>
2026-01-21 15:13:07 -0800

    End call

--- line/v02/tools.py
+++ line/v02/tools.py
@@ -4,13 +4,15 @@ Provide standard tools that can be used by all agents.
 
 from line.v02.llm import passthrough_tool
 from line.v02.events import AgentSendText, AgentEndCall
+from typing import Annotated, Optional
+from line.v02.llm.function_tool import Field
 
 __all__ = [
     "end_call",
 ]
 
 @passthrough_tool()
-    """End the call."""
 async def end_call(ctx, message: Annotated[Optional[str], Field(description="The message to say before ending the call")]):
+    """End the call."""
     yield AgentSendText(text=message)
     yield AgentEndCall()
