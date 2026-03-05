2f6a60d6b41deab248de9f1c4bda2c599ce2f87a
Lucy Liu <masssb1@gmail.com>
2026-03-05 09:51:05 -0800

    comments

--- tests/test_llm_agent_tools_system.py
+++ tests/test_llm_agent_tools_system.py
@@ -176,8 +176,8 @@ async def test_send_dtmf_hash(mock_ctx, anyio_backend):
 async def test_end_call_default_description(mock_ctx, anyio_backend):
     """Test that default end_call has a conservative default description."""
     assert end_call.description == EndCallTool.DEFAULT_DESCRIPTION
-    assert "Call when ALL of the following are true:" in end_call.description
-    assert "Do not call when:" in end_call.description
+    assert "Use when:" in end_call.description
+    assert "Don't use when:" in end_call.description
 
 
 async def test_end_call_yields_agent_end_call(mock_ctx, anyio_backend):
